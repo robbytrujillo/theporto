829854ad3fa553183c4997f3d7485b3994ef7b21
robbytrujillo <robbyikusuma88@gmail.com>
2025-07-25 11:02:17 +0700

    Introduction thePorto Project

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,9 @@
 const scrollUp = document.querySelector(".scroll-up");
 
 window.onscroll = () => {
-  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    scrollUp.classList.add("scroll-active");
+  } else {
+    scrollUp.classList.remove("scroll-active");
+  }
 };
